a9c567d337755ba75300574d0191bd17c57b026f
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-07-21 14:15:10 +0300

    adding sorting methods from lab 2

--- js/app.js
+++ js/app.js
@@ -4,10 +4,11 @@
 // Creating constructor for images objects
 var imageArr = [];
 var optionsArr = [];
-function ImageObj(image_url, keyword, title) {
+function ImageObj(image_url, keyword, title, horns) {
   this.image_url = image_url;
   this.keyword = keyword;
   this.title = title;
+  this.horns = horns;
   imageArr.push(this);
   optionsArr.push(keyword);
 }
@@ -15,7 +16,7 @@ function ImageObj(image_url, keyword, title) {
 // Get that data form json file and create new instance
 $.get('data/page-1.json', function (data) {
   data.forEach((e) => {
-    let newImage = new ImageObj(e.image_url, e.keyword, e.title);
+    let newImage = new ImageObj(e.image_url, e.keyword, e.title, e.horns);
     newImage.render();
   });
   $('#photo-template').hide();
@@ -24,14 +25,13 @@ $.get('data/page-1.json', function (data) {
 
 // function to render the images
 ImageObj.prototype.render = function () {
-  let imageTemplate = $('#photo-template').clone().removeAttr('id');
+  let imageTemplate = $('#photo-template').clone().removeAttr('id style');
   imageTemplate.attr('class', this.keyword);
   imageTemplate.find('h2').text(this.keyword);
   imageTemplate.find('img').attr('src', this.image_url);
   imageTemplate.find('p').text(this.title);
   imageTemplate.appendTo('main');
 };
-
 
 // creating options for select input
 function createOptions(arr) {
@@ -48,5 +48,31 @@ function showSelected() {
     let selected = $(this).val();
     $('section').hide();
     $(`.${selected}`).show();
+  });
+}
+
+//sort event listner
+$('#byKeyword').click(sortByKeyword);
+$('#byHorns').click(sortByHorns);
+
+// sort by keyword
+function sortByKeyword() {
+  imageArr.sort(function (a, b) {
+    return a.keyword.localeCompare(b.keyword);
+  });
+  $('main > *:not(#photo-template)').remove();
+  imageArr.forEach((e) => {
+    e.render();
+  });
+}
+
+// sort by horns
+function sortByHorns() {
+  imageArr.sort(function (a, b) {
+    return a.horns - b.horns;
+  });
+  $('main > *:not(#photo-template)').remove();
+  imageArr.forEach((e) => {
+    e.render();
   });
 }
